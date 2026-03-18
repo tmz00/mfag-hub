dfec4c8b03d7f99f12b075a0707251d41fb6e53c
tmz00 <tarmizi.bin.ahmad@gmail.com>
2026-03-18 18:22:03 +0800

    Fix api test

--- api/tests/TestCase.php
+++ api/tests/TestCase.php
@@ -6,5 +6,12 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    public function call($method, $uri, $parameters = [], $cookies = [], $files = [], $server = [], $content = null)
+    {
+        if (is_string($uri) && str_starts_with($uri, '/api')) {
+            $uri = substr($uri, 4) ?: '/';
+        }
+
+        return parent::call($method, $uri, $parameters, $cookies, $files, $server, $content);
+    }
 }
